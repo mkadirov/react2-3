0d0722b253b7c9732fc65e4be2a94690c41d31dc
Kadirov1993 <mkodirjonov@mail.ru>
2023-07-05 23:21:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import getUserList from './data/Users';
 import Foods from './pages/Foods/Foods';
 import getFoodListData from './data/FoodListData';
 import HomePage from './pages/HomePage/HomePage';
+import Page404 from './pages/Page404/Page404';
 
 
 function App() {
@@ -52,7 +53,7 @@ function App() {
 
           </Route>
            }
-           
+           <Route path='*' element = {<Page404/>}/>
         </Routes>
     </Router>
     </>
---
+++ src/pages/Page404/Page404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Page404() {
+  return (
+    <section className="my-5 py-5 ">
+        <div className="container my-5 py-5 d-flex justify-content-center ">
+           <div>
+           <h1 className="display-1 mt-5 text-danger">
+                404
+            </h1>
+            <h3 className='ms-5'>Sahifa topilmadi</h3>
+           </div>
+        </div>
+    </section>
+  )
+}
